1ea57f17fca57488b2f992318ab4c800f791ba49
Bjeck <bjarke@larsen-dk.dk>
2017-01-20 21:51:12 +0100

    frequency analysis :D

---
+++ Assets/Scripts/microphonetest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class microphonetest : MonoBehaviour {
+
+	public AudioSource aus;
+	string MicString = "Built-in Microphone";
+	float[] samples;
+	int numSamples = 64;
+	// Use this for initialization
+	void Start () {
+		samples = new float[numSamples];
+
+		aus = GetComponent<AudioSource> ();
+		aus.clip = Microphone.Start (MicString, true, 1, 44100);
+		aus.Play ();
+		aus.loop = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (aus.volume);
+
+		aus.GetSpectrumData (samples, 0,FFTWindow.BlackmanHarris);
+		print (samples[21]);
+	}
+}
