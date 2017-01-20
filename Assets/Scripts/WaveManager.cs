a701720d351319dec1c42cc10c9076eee58fc827
Mr. Hangover <ma.nygaard@webspeed.dk>
2017-01-20 19:34:58 +0100

    Initial commit!

---
+++ Assets/Scripts/WaveManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveManager : MonoBehaviour {
+
+    public static WaveManager instance = null;
+
+    List<List<GameObject>> waves = new List<List<GameObject>>();
+
+	// Use this for initialization
+	void Awake () {
+		if(instance == null)
+        {
+            instance = this;
+        }
+        else if(instance != this)
+        {
+            Destroy(gameObject);
+        }
+        DontDestroyOnLoad(gameObject);
+	}
+	
+    void Start()
+    {
+        Debug.Log(waves[0].Count);
+    }
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void AddWaveObject(int layer, GameObject wave)
+    {
+        while(waves.Count < layer)
+        {
+            waves.Add(new List<GameObject>());
+        }
+
+        waves[layer - 1].Add(wave);
+    }
+}
---
+++ Assets/Scripts/WaveObject.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveObject : MonoBehaviour {
+
+    [Range(1, 1000)]
+    public int waveLayer = 1;
+
+	// Use this for initialization
+	void Awake () {
+        WaveManager.instance.AddWaveObject(waveLayer, gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
