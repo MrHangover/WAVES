709c473eaf43830fe48b33fcb2db1196a3cce589
Bjeck <bjarke@larsen-dk.dk>
2017-01-21 19:40:29 +0100

    level again

---
+++ Assets/Scripts/LevelScroll.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelScroll : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position -= Vector3.right * WaveManager.instance.scrollSpeed * Time.fixedDeltaTime;	
+	}
+}
