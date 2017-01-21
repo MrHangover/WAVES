6a5ed9bd40d3bff7666cd78f6323a5a53812710c
Mr. Hangover <ma.nygaard@webspeed.dk>
2017-01-22 00:25:53 +0100

    Fixed some positions and made Hermes rotate like a hamster ball!

---
+++ Assets/Scripts/HermesRotation.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HermesRotation : MonoBehaviour {
+
+    const float ROTATION_SPEED = 35f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0f, 0f, -WaveManager.instance.scrollSpeed * ROTATION_SPEED * Time.deltaTime);
+	}
+}
