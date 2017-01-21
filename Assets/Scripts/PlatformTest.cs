b386cd57c98038f61c1793b9f54856cc1e08f547
Mr. Hangover <ma.nygaard@webspeed.dk>
2017-01-21 01:43:45 +0100

    Saving my changes before I mess up

---
+++ Assets/Scripts/PlatformTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformTest : MonoBehaviour {
+
+    Rigidbody2D body;
+    public float speed = 5f;
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        body.MovePosition(new Vector2(1f, 1f));
+    }
+
+    void FixedUpdate()
+    {
+    }
+}
--- Assets/Scripts/WaveObject.cs
+++ Assets/Scripts/WaveObject.cs
@@ -7,15 +7,25 @@ public class WaveObject : MonoBehaviour {
     [Range(1, 1000)]
     public int waveLayer = 1;
     public float startYPos;
+    public Rigidbody2D body;
+
+    Vector3 previousPos;
 
 	// Use this for initialization
 	void Awake () {
         //WaveManager.instance.AddWaveObject(waveLayer, gameObject);
         startYPos = transform.position.y;
+        previousPos = transform.position;
     }
 
     void Start()
     {
+        body = GetComponent<Rigidbody2D>();
+    }
 
+    void FixedUpdate()
+    {
+        body.velocity = (transform.position - previousPos) * 5f;
+        previousPos = transform.position;
     }
 }
